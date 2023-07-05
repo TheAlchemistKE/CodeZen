eebe0de31fdbf5c53d3746549a795db2e614b02a
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2023-07-05 14:55:40 +0300

    rewrite the editor to use monaco theme

--- src/App.js
+++ src/App.js
@@ -1,45 +1,8 @@
-import React, { useState } from "react";
-
-import { Dropdown } from "./components/Dropdown";
-import { Editor } from "./components/Editor";
-import { Highlighter } from "./components/Highlighter";
-
-import * as themes from "react-syntax-highlighter/dist/esm/styles/hljs";
-import * as languages from "react-syntax-highlighter/dist/esm/languages/hljs";
-
 import "./App.css";
+import Landing from "./components/Landing";
 
-const defaultLanguage = <code>${"javascript" || Object.keys(languages).sort()[0]}</code>;
-const defaultTheme = <code>${"atomOneDark" || Object.keys(themes).sort()[0]}</code>;
-
-export default function App() {
-  const [input, setInput] = useState("");
-  const [language, setLanguage] = useState(defaultLanguage);
-  const [theme, setTheme] = useState(defaultTheme);
-
-  return (
-    <div className="App">
-      <div className="ControlsBox">
-        <Dropdown
-          defaultTheme={defaultLanguage}
-          onChange={(e) => setLanguage(e.target.value)}
-          data={languages}
-        />
-        <Dropdown
-          defaultTheme={defaultTheme}
-          onChange={(e) => setTheme(e.target.value)}
-          data={themes}
-        />
-      </div>
-      <div className="PanelsBox">
-        <Editor
-          placeHolder="Type your code here..."
-          onChange={(e) => setInput(e.target.value)}
-        />
-        <Highlighter language={language} theme={themes[theme]}>
-          {input}
-        </Highlighter>
-      </div>
-    </div>
-  );
+function App() {
+  return <Landing />;
 }
+
+export default App;
